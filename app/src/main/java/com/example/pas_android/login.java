b11ae2b1e76ca3488dc667e74071d058b4ac1c47
DESKTOP-0B011BK\ASUS <you@example.com>
2020-12-03 13:40:35 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pas_android/login.java
@@ -0,0 +1,23 @@
+package com.example.pas_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+public class login extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+    }
+}
